7bb43780b91bb0d33472351cf1671a10382053ba
Pierre <fritsch@puzzle.ch>
2014-11-19 11:17:26 +0100

    Add Magellan

--- app/js/app.js
+++ app/js/app.js
@@ -1,25 +1,25 @@
 var app = (function(document, $) {
 
-	'use strict';
-	var docElem = document.documentElement,
+  'use strict';
+  var docElem = document.documentElement,
 
-		_userAgentInit = function() {
-			docElem.setAttribute('data-useragent', navigator.userAgent);
-		},
-		_init = function() {
-			$(document).foundation();
-			_userAgentInit();
-		};
+    _userAgentInit = function() {
+      docElem.setAttribute('data-useragent', navigator.userAgent);
+    },
+    _init = function() {
+      $(document).foundation();
+      _userAgentInit();
+    };
 
-	return {
-		init: _init
-	};
+  return {
+    init: _init
+  };
 
 })(document, jQuery);
 
 (function() {
 
-	'use strict';
-	app.init();
+  'use strict';
+  app.init();
 
 })();
