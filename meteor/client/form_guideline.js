8ca5f7776b1ea47a7f1d8a478b549d2eb27bbf08
Pierre <fritsch@puzzle.ch>
2014-12-01 17:17:07 +0100

    Questions types guideline WIP

---
+++ meteor/client/form_guideline.js
@@ -0,0 +1,24 @@
+Template.form_guideline.helpers({
+  stepCompleted: function(){
+    return Session.get('stepCompleted');
+  },
+  progress: function(stepNb){
+    var stepCompleted = Session.get('stepCompleted');
+    var currentProgress = (100 * stepCompleted)/stepNb;
+    return currentProgress;
+  }
+});
+
+Session.set("stepCompleted", 0);
+
+Template.form_guideline.events({
+  "blur textarea, blur input[type='text'], change checkbox": function (event, template) {
+    var content = event.target.value;
+    var step = Session.get('stepCompleted');
+    if(content != '' && step < 3) {
+      Session.set('stepCompleted', step + 1);
+    } else if(step > 0) {
+      Session.set('stepCompleted', step - 1);
+    }
+  }
+});
